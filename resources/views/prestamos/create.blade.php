3cbea82b99d94dc040ce81ca135e5ebeef4dd7f7
Jose Manuel Perez Carrasco <33356314+roulez@users.noreply.github.com>
2023-09-23 17:00:23 +0200

    Made sure to forbid the access to all screnss related to loans to non authoraized users

--- resources/views/prestamos/create.blade.php
+++ resources/views/prestamos/create.blade.php
@@ -1,41 +1,45 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-12 pt-2">
-            <a href="/prestamos" class="btn btn-outline-primary bt-sm">Volver</a>
-            <div class="border rounded mt-5 pl-4 pr-4 pt-4 pb-4">
-                <h1 class="display-one">Registrar Prestamo</h1>
-                <form action="" method="POST">
-                    @csrf
-                    <div class="row">
-                        <div class="control-group col-12">
-                            <label for="user_id">Id Usuario</label>
-                            <input type="number" id='user_id' class="form-control" name="user_id" placeholder="Id del usuario..." required>
-                        </div>
-                        <div class="control-group col-12 mt-2">
-                            <label for="book_id">Id Libro</label>
-                            <input type="number" id='book_id' class="form-control" name="book_id" placeholder="Id del libro..." required>
-                        </div>
-                        <div class="control-group col-12 mt-2">
-                            <label for="fecha_prestamo">Fecha del prestamo</label>
-                            <input type="number" id='fecha_prestamo' class="form-control" name="fecha_prestamo" placeholder="Fecha del prestamo del libro..." required>
-                        </div>
-                        <div class="control-group col-12 mt-2">
-                            <label for="fecha_devolucion">Fecha de devolucion</label>
-                            <input type="number" id='fecha_devolucion' class="form-control" name="fecha_devolucion" placeholder="Fecha de devolucion del prestamo..." required>
-                        </div>
-                    </div>
-                    @if (Auth::user())
-                        <div class="row mt-2">
-                            <div class="control-group col-12 text-center">
-                                <button class="btn bt-primary">Crear Prestamo</button>
+@if (Auth::user())
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pt-2">
+                <a href="/prestamos" class="btn btn-outline-primary bt-sm">Volver</a>
+                <div class="border rounded mt-5 pl-4 pr-4 pt-4 pb-4">
+                    <h1 class="display-one">Registrar Prestamo</h1>
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="control-group col-12">
+                                <label for="user_id">Id Usuario</label>
+                                <input type="number" id='user_id' class="form-control" name="user_id" placeholder="Id del usuario..." required>
+                            </div>
+                            <div class="control-group col-12 mt-2">
+                                <label for="book_id">Id Libro</label>
+                                <input type="number" id='book_id' class="form-control" name="book_id" placeholder="Id del libro..." required>
+                            </div>
+                            <div class="control-group col-12 mt-2">
+                                <label for="fecha_prestamo">Fecha del prestamo</label>
+                                <input type="number" id='fecha_prestamo' class="form-control" name="fecha_prestamo" placeholder="Fecha del prestamo del libro..." required>
+                            </div>
+                            <div class="control-group col-12 mt-2">
+                                <label for="fecha_devolucion">Fecha de devolucion</label>
+                                <input type="number" id='fecha_devolucion' class="form-control" name="fecha_devolucion" placeholder="Fecha de devolucion del prestamo..." required>
                             </div>
                         </div>
-                    @endif
-                </form>
-            </div>         
+
+                            <div class="row mt-2">
+                                <div class="control-group col-12 text-center">
+                                    <button class="btn bt-primary">Crear Prestamo</button>
+                                </div>
+                            </div>
+
+                    </form>
+                </div>         
+            </div>
         </div>
     </div>
-</div>
+@else
+    <p>No puede acceder a los prestamos a menos que se registre primero.</p>
+@endif
 @endsection
--- resources/views/prestamos/edit.blade.php
+++ resources/views/prestamos/edit.blade.php
@@ -1,27 +1,31 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-12 pt-2">
-            <a href="/prestamos" class="btn btn-outline-primary bt-sm">Volver</a>
-            <div class="border rounded mt-5 pl-4 pr-4 pt-4 pb-4">
-                <h1 class="display-one">Editar Prestamo</h1>
-                <form action="" method="POST">
-                    @csrf
-                    <div class="row">
-                        <div class="control-group col-12 mt-2">
-                            <label for="fecha_devolucion">Fecha de devolucion</label>
-                            <input type="number" id='fecha_devolucion' class="form-control" name="fecha_devolucion" value="{{$prestamo->fecha_devolucion}}" placeholder="Fecha de devolucion del prestamo..." required>
+@if (Auth::user())
+    <div class="container">
+        <div class="row">
+            <div class="col-12 pt-2">
+                <a href="/prestamos" class="btn btn-outline-primary bt-sm">Volver</a>
+                <div class="border rounded mt-5 pl-4 pr-4 pt-4 pb-4">
+                    <h1 class="display-one">Editar Prestamo</h1>
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="control-group col-12 mt-2">
+                                <label for="fecha_devolucion">Fecha de devolucion</label>
+                                <input type="number" id='fecha_devolucion' class="form-control" name="fecha_devolucion" value="{{$prestamo->fecha_devolucion}}" placeholder="Fecha de devolucion del prestamo..." required>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row mt-2">
-                        <div class="control-group col-12 text-center">
-                            <button class="btn bt-primary">Editar Prestamo</button>
+                        <div class="row mt-2">
+                            <div class="control-group col-12 text-center">
+                                <button class="btn bt-primary">Editar Prestamo</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </div>         
+                    </form>
+                </div>         
+            </div>
         </div>
     </div>
-</div>
+@else
+    <p>No puede acceder a los prestamos a menos que se registre primero.</p>
+@endif
 @endsection
